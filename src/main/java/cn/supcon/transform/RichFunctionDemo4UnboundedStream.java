0ff8ed249b63967d39ab38f57b69f228f3670221
liuwenzhi <liuwenzhi@supconit.com>
2024-04-18 10:07:27 +0800

    ADD：增加无界流富函数类相关代码

--- src/main/java/cn/supcon/transform/RichFunctionDemo4UnboundedStream.java
+++ src/main/java/cn/supcon/transform/RichFunctionDemo4UnboundedStream.java
@@ -9,16 +9,15 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
  * 富函数类
- * 本类通过有界流进行测试
+ * 本类通过无界流进行测试
  *
  * Flink1.17 44课
  */
-public class RichFunctionDemo {
+public class RichFunctionDemo4UnboundedStream {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        DataStreamSource<Integer> source = env.fromElements(1, 2, 3, 4);
-        SingleOutputStreamOperator<Integer> map = source.map(new RichMapFunction<Integer, Integer>() {
-
+        DataStreamSource<String> source = env.socketTextStream("data62", 7777);
+        SingleOutputStreamOperator<Integer> map = source.map(new RichMapFunction<String, Integer>() {
             // 富函数中的生命周期函数,open函数在map函数之前被调用
             @Override
             public void open(Configuration parameters) throws Exception {
@@ -30,7 +29,10 @@ public class RichFunctionDemo {
                 System.out.println("open函数开始执行,子任务id:" + indexOfThisSubtask + ",子任务名称:" + taskNameWithSubtasks);
             }
 
-            // 富函数中的生命周期函数，close函数在map函数之后被调用
+            /* 富函数中的生命周期函数，close函数在程序正常退出时，map函数之后被调用，正常退出情况包括：无界流中断，程序正常退出，flink运维界面点cancel
+            *  程序异常退出包括：代码抛出异常（比如调用map算子的时候，抛出了类型转换异常），关闭服务等
+             */
+
             @Override
             public void close() throws Exception {
                 super.close();
@@ -41,8 +43,8 @@ public class RichFunctionDemo {
             }
 
             @Override
-            public Integer map(Integer value) throws Exception {
-                return value + 1;
+            public Integer map(String value) throws Exception {
+                return Integer.parseInt(value) + 1;
             }
         });
         map.print();
