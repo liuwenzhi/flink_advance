81726b1ea6b377b5e351ba428a2b07d3a998e81a
liuwenzhi <liuwenzhi87@sina.cn>
2024-04-29 14:49:07 +0800

    ADD：增加54课到57课kafka写入kafka算子相关代码

--- src/main/java/cn/supcon/sink/FileSinkDemo.java
+++ src/main/java/cn/supcon/sink/FileSinkDemo.java
@@ -1,12 +1,24 @@
 package cn.supcon.sink;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.connector.source.util.ratelimit.RateLimiterStrategy;
+import org.apache.flink.configuration.MemorySize;
 import org.apache.flink.connector.datagen.source.DataGeneratorSource;
 import org.apache.flink.connector.datagen.source.GeneratorFunction;
+import org.apache.flink.connector.file.sink.FileSink;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.filesystem.OutputFileConfig;
+import org.apache.flink.streaming.api.functions.sink.filesystem.bucketassigners.DateTimeBucketAssigner;
+import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
+
+import java.time.Duration;
+import java.time.ZoneId;
+
 
 /**
  * 写入文件的sink算子
@@ -16,16 +28,29 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 public class FileSinkDemo {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        // 每个目录中，都会有并行度个数的文件在写入
         env.setParallelism(2);
+        // 2秒钟关闭一次文件，避免文件出现in progress的情况
+        env.enableCheckpointing(2000, CheckpointingMode.EXACTLY_ONCE);
         DataGeneratorSource dataGeneratorSource = new DataGeneratorSource<>(new GeneratorFunction<Long, String>() {
             @Override
             public String map(Long value) throws Exception {
                 return "Number:" + value;
             }
-        }, 100,
+        }, Long.MAX_VALUE,
                 RateLimiterStrategy.perSecond(1),
                 Types.STRING);
         DataStreamSource dataGen = env.fromSource(dataGeneratorSource, WatermarkStrategy.noWatermarks(), "data-generator");
+        // 按照行输出
+        FileSink<String> fileSink = FileSink.<String>forRowFormat(new Path("f:/tmp"), new SimpleStringEncoder<>("UTF-8"))
+                // 输出一些文件的配置，前缀，后缀名称等
+                .withOutputFileConfig(OutputFileConfig.builder().withPartPrefix("abc-").withPartSuffix(".log").build())
+                // 指定分桶策略，分桶即是分文件，这里设计根据时间分文件
+                .withBucketAssigner(new DateTimeBucketAssigner<>("yyyy-MM-dd--HH", ZoneId.systemDefault()))
+                // 指定文件的滚动策略，根据时间和文件大小两个方面设置，下边设置为10秒或者1M创建一次新文件
+                .withRollingPolicy(DefaultRollingPolicy.builder().withRolloverInterval(Duration.ofSeconds(10)).withMaxPartSize(new MemorySize(1024 * 1024)).build())
+                .build();
+        dataGen.sinkTo(fileSink);
         env.execute();
     }
 }
--- src/main/java/cn/supcon/sink/SinkKafkaWithKey.java
+++ src/main/java/cn/supcon/sink/SinkKafkaWithKey.java
@@ -1,5 +1,6 @@
 package cn.supcon.sink;
 
+import org.apache.flink.api.common.serialization.SerializationSchema;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.connector.base.DeliveryGuarantee;
 import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
@@ -8,18 +9,28 @@ import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import javax.annotation.Nullable;
+import java.nio.charset.StandardCharsets;
 
 /**
- * 写数据到kafka目的端
- * 代码使用精准一次方式写入到kafka，如果不设置精准一次写入，可以把注释说明精准一次部分的代码去掉
- * 注意：如果要使用精准一次 写入kafka，需要满足一下条件，缺一不可：
- * 1、开启checkpoint（后续课程介绍）
- * 2、设置事务前缀
- * 3、设置事务超时时间：  checkpoint间隔  < 事务超时时间  <  max的15分钟
+ * KafkaSinkDemo的补充
+ * 自定义序列化器，包括对kafka的key和value值的序列化
+ * 之前传输的测试数据类似这种：
+ * 1,1,a
+ * 2,2,b
+ * 3,3,c
+ * 序列化也是针对于数据本身，现在是给这个数据加一个key，比如1,1,a的key是1,然后给这个key再做一次序列化
+ * 具体思路：自定义一个序列化器：
+ * 1、实现一个接口，重写序列化方法
+ * 2、指定key，转成字节数组
+ * 3、指定value，转成字节数组
+ * 4、返回一个ProducerRecord对象，把key、value放进去
  *
- * Flink 1.17 55课
+ * Flink 1.17 56课
  */
-public class KafkaSinkDemo {
+public class SinkKafkaWithKey {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
@@ -28,10 +39,17 @@ public class KafkaSinkDemo {
         SingleOutputStreamOperator<String> sensorDS = env.socketTextStream("172.16.2.23", 7777);
         // 自定义一个kafka sink
         KafkaSink<String> kafkaSink = KafkaSink.<String>builder().setBootstrapServers("172.16.2.62:9092").setRecordSerializer(
-                        KafkaRecordSerializationSchema.<String>builder()
-                                .setTopic("topic_test")
-                                .setValueSerializationSchema(new SimpleStringSchema())   // 设置value序列化器
-                                .build())
+                        // 通过匿名实现类的方式实现该接口
+                        new KafkaRecordSerializationSchema<String>() {
+                            @Nullable
+                            @Override
+                            public ProducerRecord<byte[], byte[]> serialize(String element, KafkaSinkContext kafkaSinkContext, Long aLong) {
+                                String[] datas = element.split(",");
+                                byte[] key = datas[0].getBytes(StandardCharsets.UTF_8);
+                                byte[] value = element.getBytes(StandardCharsets.UTF_8);
+                                return new ProducerRecord<>("topic_test", key, value);
+                            }
+                        })
                 .setDeliveryGuarantee(DeliveryGuarantee.EXACTLY_ONCE) // 设置发送保证，kafka一致性级别（精准一次，至少一次等），如果是精准一次，必须要设置事务前缀
                 .setTransactionalIdPrefix("supcon-")
                 .setProperty(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, 10 * 60 * 1000 + "") // 精准一次写入必须要设置事务超时时间，大于checkpoint时间间隔，不超过15分钟的一个时间信息，单位是毫秒
